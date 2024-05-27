3cfc989a33f51e96a95d099a8227952a6cffd7aa
shahjalal132 <rjshahjalal132@gmail.com>
2024-05-27 11:29:41 +0600

    enqueue bootstrap files

--- inc/classes/class-enqueue-assets.php
+++ inc/classes/class-enqueue-assets.php
@@ -19,18 +19,22 @@ class Enqueue_Assets {
 
     public function enqueue_css() {
         // Register CSS
-        wp_register_style( "style", BULK_PRODUCT_IMPORT_ASSETS_PATH . "/css/be-style.css", [], false, "all" );
+        wp_register_style( "be-style", BULK_PRODUCT_IMPORT_ASSETS_PATH . "/css/be-style.css", [], false, "all" );
+        wp_register_style( "be-bootstrap", BULK_PRODUCT_IMPORT_ASSETS_PATH . "/css/bootstrap.min.css", [], false, "all" );
 
         // enqueue CSS
-        wp_enqueue_style( "style" );
+        wp_enqueue_style( "be-style" );
+        wp_enqueue_style( "be-bootstrap" );
     }
 
     public function enqueue_js() {
 
         // Register JS
-        wp_register_script( "app", BULK_PRODUCT_IMPORT_ASSETS_PATH . "/js/app.js", [ 'jquery' ], false, true );
+        wp_register_script( "be-app", BULK_PRODUCT_IMPORT_ASSETS_PATH . "/js/app.js", [ 'jquery' ], false, true );
+        wp_register_script( "be-bootstrap", BULK_PRODUCT_IMPORT_ASSETS_PATH . "/js/bootstrap.bundle.min.js", [], false, true );
 
         // enqueue JS
-        wp_enqueue_script( "app" );
+        wp_enqueue_script( "be-app" );
+        wp_enqueue_script( "be-bootstrap" );
     }
 }
