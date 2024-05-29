f848ba80314704ea933e49153ddf4891667656ec
shahjalal132 <rjshahjalal132@gmail.com>
2024-05-29 12:32:52 +0600

    added file-insert-products-db.php file

--- load.php
+++ load.php
@@ -5,7 +5,9 @@
  * all files in this folder will be loaded
  */
 
-// load db tables create file
+defined( "ABSPATH" ) || exit( "Direct Access Not Allowed" );
+
 require_once BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/inc/files/file-db-table-create.php';
 require_once BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/inc/files/file-import-products-woo.php';
+require_once BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/inc/files/file-insert-products-db.php';
 require_once BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/inc/files/file-api-endpoints.php';
