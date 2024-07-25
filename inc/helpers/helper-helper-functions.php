390298cecac04b5d480e71f7167c60d1ea44e4fc
shahjalal132 <rjshahjalal132@gmail.com>
2024-07-25 11:05:53 +0600

    update helper-functions.php file

--- inc/helpers/helper-helper-functions.php
+++ inc/helpers/helper-helper-functions.php
@@ -4,7 +4,7 @@
  * Dump and die
  *
  * @param string $value
- * @return void
+ * @return never
  */
 function dd( $value ) {
     var_dump( $value );
@@ -15,7 +15,7 @@ function dd( $value ) {
  * Print and die
  *
  * @param array $value
- * @return void
+ * @return never
  */
 function pd( $value ) {
     echo '<pre>';
@@ -28,16 +28,16 @@ function pd( $value ) {
  *
  * @param string $data The response data to be saved.
  */
-function put_api_response_data( $data ) {
+function put_program_logs( $data ) {
 
     // Ensure directory exists to store response data
-    $directory = BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/api_response/';
+    $directory = BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/program_logs/';
     if ( !file_exists( $directory ) ) {
         mkdir( $directory, 0777, true );
     }
 
     // Construct file path for response data
-    $file_name = $directory . 'response.log';
+    $file_name = $directory . 'program_logs.log';
 
     // Get the current date and time
     $current_datetime = date( 'Y-m-d H:i:s' );
@@ -50,36 +50,5 @@ function put_api_response_data( $data ) {
         return "Data appended to file successfully.";
     } else {
         return "Failed to append data to file.";
-    }
-}
-
-/**
- * Write an error message to a file.
- *
- * @param string $error_message The error message to be written to the file.
- * @return string Return a success or failure message.
- */
-function put_api_error_message( $error_message ) {
-
-    // Ensure directory exists to store error logs
-    $directory = BULK_PRODUCT_IMPORT_PLUGIN_PATH . '/api_response/';
-    if ( !file_exists( $directory ) ) {
-        mkdir( $directory, 0777, true );
-    }
-
-    // Construct file path for error log
-    $file_name = $directory . 'error-log.log';
-
-    // Get the current date and time
-    $current_datetime = date( 'Y-m-d H:i:s' );
-
-    // Append current date and time to the error message
-    $error_message = $error_message . ' - ' . $current_datetime;
-
-    // Append new error message to the existing file
-    if ( file_put_contents( $file_name, $error_message . "\n\n", FILE_APPEND | LOCK_EX ) !== false ) {
-        return "Error message appended to file successfully.";
-    } else {
-        return "Failed to append error message to file.";
     }
 }
